c403ada456f63cfffca1c9a5fca026d91da7b150
tgalvinjr <tgalvin.jr@gmail.com>
2019-04-08 16:20:35 +0300

    add global variables on scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//defining global variables
+$().ready(function(){
+var diceRoll1 = 0;
+var diceRoll2 = 0;
+var totalScore1 = 0;
+var totalScore2 = 0;
+//button click event
+$("#roll1").click(function(){
+})
+})
